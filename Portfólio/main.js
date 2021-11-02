507c954735696d2b3116e59fd5766a5bf6edc39d
rogerch93 <rogerinhochaves@hotmail.com>
2021-11-02 09:40:58 -0300

    adc nav bar/efects

---
+++ Portfólio/main.js
@@ -0,0 +1,11 @@
+/*efeito de escrita automatica*/
+function write(elemento){
+  const textoArray = elemento.innerHTML.split('');
+  elemento.innerHTML = '';
+  textoArray.forEach((letra,i) =>{
+    setTimeout(() => elemento.innerHTML += letra , 75 *i);
+  });
+}
+
+const titulo = document.querySelector('h1');
+write(titulo);
